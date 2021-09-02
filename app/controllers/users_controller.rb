23583b77791cae431576fcfdd53e117f4302e11a
Lena Hou <lenahou2013@gmail.com>
2021-09-02 12:42:42 -0400

    add .env

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(
+            :username,
+            :email,
+            :password,
+            :password_confirmation
+        )
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates_presence_of :email
+     
 end
