31d4daa941d93941883a219a095e85c0635f6013
Lena Hou <lenahou2013@gmail.com>
2021-09-03 15:59:53 -0400

    clean up

--- app/helpers/measurements_helper.rb
+++ app/helpers/measurements_helper.rb
@@ -1,6 +1,6 @@
 module MeasurementsHelper
 
-    def form_url_helper(drink)
+    def measurement_form_url_helper(drink)
         drink ? drink_measurements_path(drink) : measurements_path
     end
 end
