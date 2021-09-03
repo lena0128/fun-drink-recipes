53be4ead67ccbd5a45ff3d08b4c7dda03f4e0d4a
Lena Hou <lenahou2013@gmail.com>
2021-09-03 13:28:44 -0400

    nested review resource on ingredient resrouce

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,30 @@
 class ReviewsController < ApplicationController
     before_action :require_login
 
+    def index
+        @reviews = Review.all
+    end
+
     def new
-        @review = Review.new
+        if params[:ingredient_id]
+            @ingredient = Ingredient.find_by(id: params[:ingredient_id])
+            @review = @ingredient.reviews.build(user: current_user)
+            @ingredients = Ingredient.all
+        else
+            @review = Review.new
+        end
     end
 
     def create
-        @review = Review.create(review_params)
-        @review.user_id = current_user.id 
+        @review = Review.new(review_params)
+        @review.user_id = current_user.id
+        if params[:ingredient_id]
+            @review.ingredient_id = params[:ingredient_id]
+            @ingredient = Ingredient.find_by(id: params[:ingredient_id])
+        end
         if @review.save
             flash[:message] = "Your review has been successfully created!"
-            redirect_to review_path(@review)
+            redirect_to ingredient_path(@ingredient)
         else
             render :new
         end
@@ -25,6 +39,7 @@ class ReviewsController < ApplicationController
         params.require(:review).permit(
             :rating,
             :content,
+            :ingredient_id,
             :user_id
         )
       end
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,6 @@
 module ReviewsHelper
+    
+    def form_url_helper(ingredient)
+      ingredient ? ingredient_reviews_path : reviews_path
+    end
 end
