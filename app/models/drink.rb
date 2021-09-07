f6dfd3195967a4a2cbc7fd136e3fc4e1a2ef0845
Lena Hou <lenahou2013@gmail.com>
2021-09-07 14:44:00 -0400

    refactor filter of drinks

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -12,16 +12,7 @@ class Drink < ApplicationRecord
     scope :drink_search, ->(name) { where( "drink_name LIKE ?", "%#{name.titlecase}%")}
     
     # filter by category
-    scope :filter_ordinary_drink, -> { where(category: "Ordinary Drink")}
-    scope :filter_cocktail, -> { where(category: "Cocktail")}
-    scope :filter_milk_float_shake, -> { where(category: "Milk / Float / Shake")}
-    scope :filter_cocoa, -> { where(category: "Cocoa")}
-    scope :filter_shot, -> { where(category: "Shot")}
-    scope :filter_coffee_tea, -> { where(category: "Coffee / Tea")}
-    scope :filter_homemade_liqueur, -> { where(category: "Homemade Liqueur")}
-    scope :filter_punch_party_drink, -> { where(category: "Punch / Party Drink")}
-    scope :filter_beer, -> { where(category: "Beer")}
-    scope :filter_soda, -> { where(category: "Soft Drink / Soda")}
+    scope :filter_by_category, ->(filter) {where("category = ?", filter).order(:drink_name)}
     
     # latest data displayed on hoempage
     scope :latest_alcohoic_drinks, -> { where(alcoholic: "Alcoholic").last(3)} 
